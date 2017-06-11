fcfc71e50bfb01143d263db8786f5451d4d9f969
Sergio Guillen <serguimant@gmail.com>
2017-06-11 19:27:19 -0400

    Add search query for grid edition
    Added the search query that filter news by:
    * Date range
    * Client Id

--- app/Http/Controllers/GridEditController.php
+++ app/Http/Controllers/GridEditController.php
@@ -1,7 +1,10 @@
 <?php namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\NewsDetail;
+use Carbon\Carbon;
+use Log;
+use DateTime;
 
 class GridEditController extends Controller
 {
@@ -12,8 +15,59 @@ class GridEditController extends Controller
 
     public function index(Request $r)
     {
-        return response()->json([
-            'foo' => 'bar'
+        $newsColumns = ['news.date', 'news.press_note', 'news.code', 'news.clasification', 'news_details.*'];
+
+        // Log::info("Grid Edit Search:");
+        // Log::info("From Date: $fromDate To Date: $toDate ClientId: $clientId");
+        $query = $this->getSearchQuery($r);
+
+        $news = $query->get($newsColumns);
+
+        return response()->json($news);
+    }
+
+    private function getSearchQuery(Request $r) {
+        $fromDate = $r->get('fromDate', false);
+        $toDate = $r->get('toDate', false);
+        $searchBy = $r->get('searchBy', false);
+        $clientId = $r->get('clientId', false);
+
+        $query = NewsDetail::with([
+            'news' => function($q) {
+                $q = $q->with('client');
+            }
         ]);
+        $query->join('news', 'news_details.news_id', '=', 'news.id');
+
+        $dateField = 'date';
+        if($searchBy == 'created') {
+            $dateField = 'news.created_at';
+        }
+
+        // By default search today's news
+        $now = Carbon::now();
+        $now = $now->year . '-' . $now->month . '-' . $now->day;
+        if($fromDate) {
+            $fromDate = DateTime::createFromFormat('d/m/Y', $fromDate)->format('Y-m-d');
+        } else {
+            $fromDate = $now;
+        }
+        if($toDate) {
+            $toDate = DateTime::createFromFormat('d/m/Y', $toDate)->format('Y-m-d');
+        } else {
+            $toDate = $now;
+        }
+        $query->where($dateField, '>=', $fromDate);
+        $query->where($dateField, '<=', $toDate);
+
+        // Always include coyuntura news
+        if ($clientId) {
+            $query->where(function($q) use($clientId) {
+                $q->where('client_id', '=', 100);
+                $q->orWhere('client_id', '=', $clientId);
+            });
+        }
+
+        return $query;
     }
 }
