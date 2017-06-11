ecbcd078545e3c0cda18cbff866fc7a6ca33a852
Sergio Guillen <serguimant@gmail.com>
2017-06-11 19:28:40 -0400

    Added date-picker css and js dependencies

--- resources/views/news/grid.blade.php
+++ resources/views/news/grid.blade.php
@@ -5,5 +5,11 @@
 @endsection
 
 @section('scripts')
+<script src="{{asset('assets/vendors/js/bootstrap-datepicker.min.js')}}"></script>
+<script src="{{asset('assets/vendors/js/bootstrap-datepicker.es.min.js')}}"></script>
 <script src="{{asset('assets/js/grid.bundle.js')}}"></script>
+@endsection
+
+@section('styles')
+<link rel="stylesheet" type="text/css" href="{{asset('assets/vendors/css/bootstrap-datepicker.min.css')}}">
 @endsection
